6254e44c6292d26cb722b10e682bc8055d4d53f2
SARAH BARRON <sarahibarron@hotmail.co.uk>
2023-09-14 11:00:55 +0100

    new changes

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log("Hello, world!")
+
+
+let time = prompt('What is the time?')
+
+if (time <= 11) {
+    console.log('Good morning!')
+} else if (time <= 17) {
+    console.log('It is less than 17')
+} else if (time <= 24) {
+    console.log('Is is not yet midnight')
+} else {
+    console.log("that isn't a real time!!!!")
+}
+
+let username = prompt('What is your name??')
+
+console.log("hello " + username)
+
+
+ 
+// data types
+let number = 25
+let string = "25"
+let boolean = false
